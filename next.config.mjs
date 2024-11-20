51c5947d4a00fc1763d8ad88a1993408b3e1db61
Benny Hinn <codebfullstack@gmail.com>
2024-11-20 20:46:46 +0300

    updates to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    sync rewrites() {
-        return [
-          {
-            source: '/ticko/:path*', // Frontend API route
-            destination: 'http://34.46.249.52:9001/ticko/:path*', // Backend API URL
-          },
-        ];
+  async rewrites() {
+    return [
+      {
+        source: "/ticko/:path*", // Frontend API route
+        destination: "http://104.197.183.167/ticko/:path*", // Backend API URL
       },
+    ];
+  },
 };
 
 export default nextConfig;
