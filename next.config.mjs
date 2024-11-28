ed481c6251f550302ff0de01ef5fa371511a31b5
Benny Hinn <codebfullstack@gmail.com>
2024-11-28 11:04:26 +0300

    changes...

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
     return [
       {
         source: "/ticko/:path*", // Frontend API route
-        // destination: "http://104.197.183.167/ticko/:path*", // Backend API URL
-        destination: "http://127.0.0.1:5555/ticko/:path*", // Backend API URL
+        destination: "http://34.35.77.253/ticko/:path*", // Backend API URL
+        // destination: "http://127.0.0.1:5555/ticko/:path*", // Backend API URL
       },
     ];
   },
